9be99dd34f5c3e9052d585d5f6c2bff07291a112
Jared Cox <coxj1@mcmaster.ca>
2020-08-28 20:41:31 -0400

    features from tutorial added

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: ''
+  }
 
   async componentDidMount() {
     const data = await fetchData();
+    this.setState({ data });
+  }
 
-    console.log(data);
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country })
   }
 
   render() {
+    const { data, country } = this.state;
+
     return <div className={styles.container} >
-      <Cards />
-      <CountryPicker />
-      <Chart />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={this.handleCountryChange} />
+      <Chart data={data} country={country} />
     </div>
   }
 }
